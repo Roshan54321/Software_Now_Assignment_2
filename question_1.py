52f1406af703875ebf12964d758c8b306b9f89dc
Navodyapiumanthi <navodyapiumanthi@gmail.com>
2025-08-25 01:21:55 +0930

    Added helper functions to encrypt and decrypt single letters

--- question_1.py
+++ question_1.py
@@ -1,76 +1,53 @@
-def encryption(shift1, shift2):
-    with open("resources/raw_text.txt", "r") as file:
-        text = file.read()
-
-        encrypted_text = ''
-        for letter in text:
-            # for lowercase letters
-            if 'a' <= letter <= 'z':
-                if letter <= 'm':
-                    # to make sure the shift is between a-m in a round fashion
-                    encrypted_text += chr(ord('a') + (ord(letter) - ord('a') + (shift1 * shift2)) % 13)
-                else:
-                    encrypted_text += chr(ord('n') + (ord(letter) - ord('n') - (shift1 + shift2)) % 13)
-            # for uppercase letters
-            elif 'A' <= letter <= 'Z':
-                if letter <= 'M':
-                    encrypted_text += chr(ord('A') + (ord(letter) - ord('A') - shift1) % 13)
-                else:
-                    encrypted_text += chr(ord('N') + (ord(letter) - ord('N') + (shift2 ** 2)) % 13)
-            else:
-                encrypted_text += letter
-
-    with open("resources/encrypted_text.txt", "w") as file:
-        file.write(encrypted_text)
-
-def decryption(shift1, shift2):
-    with open("resources/encrypted_text.txt", "r") as file:
-        encrypted_text = file.read()
-
-        decrypted_text = ''
-        for letter in encrypted_text:
-            # lowercase letters
-            if 'a' <= letter <= 'z':
-                if letter <= 'm':  
-                    decrypted_text += chr(ord('a') + (ord(letter) - ord('a') - (shift1 * shift2)) % 13)
-                else: 
-                    decrypted_text += chr(ord('n') + (ord(letter) - ord('n') + (shift1 + shift2)) % 13)
-            # uppercase letters
-            elif 'A' <= letter <= 'Z':
-                if letter <= 'M': 
-                    decrypted_text += chr(ord('A') + (ord(letter) - ord('A') + shift1) % 13)
-                else:
-                    decrypted_text += chr(ord('N') + (ord(letter) - ord('N') - (shift2 ** 2)) % 13)
-            else:
-                decrypted_text += letter
-
-    with open("resources/decrypted_text.txt", "w") as file:
-        file.write(decrypted_text)
-
-def verification():
-    with open("resources/raw_text.txt", "r") as file:
-        raw_text = file.read()    
-    with open("resources/decrypted_text.txt", "r") as file:
-        decrypted_text = file.read()
-    if raw_text == decrypted_text:
-        print("Encryption and decryption are consistent.")
+#Encrypt and decrypt a single letter
+def encrypt_letter(letter, shift1, shift2):
+    if 'a' <= letter <= 'z':
+        if 'a' <= letter <= 'm':
+            new_ord = ord(letter) + (shift1 * shift2)
+            if new_ord > ord('z'):
+                new_ord = ord('a') + (new_ord - ord('z') - 1)
+            return chr(new_ord)
+        else:
+            new_ord = ord(letter) - (shift1 + shift2)
+            if new_ord < ord('a'):
+                new_ord = ord('z') - (ord('a') - new_ord - 1)
+            return chr(new_ord)
+    elif 'A' <= letter <= 'Z':
+        if 'A' <= letter <= 'M':
+            new_ord = ord(letter) - shift1
+            if new_ord < ord('A'):
+                new_ord = ord('Z') - (ord('A') - new_ord - 1)
+            return chr(new_ord)
+        else:
+            new_ord = ord(letter) + (shift2 ** 2)
+            if new_ord > ord('Z'):
+                new_ord = ord('A') + (new_ord - ord('Z') - 1)
+            return chr(new_ord)
     else:
-        print("Mismatch found between original and decrypted texts.")
+        return letter
 
-def main():
-    try:
-        print("Provide two shift values separated by a space:")
-        shift1, shift2 = map(int, input().split())
 
-        encryption(shift1, shift2)
-        decryption(shift1, shift2)
-        verification()
-        
-    except ValueError:
-        print("Invalid input. Please enter two integers separated by a space.")
-
-    except Exception as e:
-        print("Please try again.")
-
-if __name__ == "__main__":
-    main()
+def decryption_letter(letter, shift1, shift2):
+    if 'a' <= letter <= 'z':
+        if 'a' <= letter <= 'm':
+            new_ord = ord(letter) - (shift1 * shift2)
+            if new_ord < ord('a'):
+                new_ord = ord('z') - (ord('a') - new_ord - 1)
+            return chr(new_ord)
+        else:
+            new_ord = ord(letter) + (shift1 + shift2)
+            if new_ord > ord('z'):
+                new_ord = ord('a') + (new_ord - ord('z') - 1)
+            return chr(new_ord)
+    elif 'A' <= letter <= 'Z':
+        if 'A' <= letter <= 'M':
+            new_ord = ord(letter) + shift1
+            if new_ord > ord('Z'):
+                new_ord = ord('A') + (new_ord - ord('Z') - 1)
+            return chr(new_ord)
+        else:
+            new_ord = ord(letter) - (shift2 ** 2)
+            if new_ord < ord('A'):
+                new_ord = ord('Z') - (ord('A') - new_ord - 1)
+            return chr(new_ord)
+    else:
+        return letter
