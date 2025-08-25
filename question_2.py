4cabc84ba1f373ea475a07ac706fee6b2b5ac769
Navodyapiumanthi <navodyapiumanthi@gmail.com>
2025-08-25 23:23:47 +0930

    recorrect

--- question_2.py
+++ question_2.py
@@ -3,36 +3,15 @@ import glob
 import os
 
 def load_temperature_data(folder_path="resources/temperatures"):
-    # Get all CSV files in the folder
     all_files = glob.glob(os.path.join(folder_path, "*.csv"))
     
-    # Read and combine all CSVs
     df_list = []
     for file in all_files:
         df = pd.read_csv(file)
         df_list.append(df)
     
-    # Combine into one DataFrame
-    combined_df = pd.concat(df_list, ignore_index=True)
-    return combined_df
-
-if __name__ == "__main__":
-    data = load_temperature_data()
-    print(data.head())  # preview first 5 rows
-
-
-def assign_season(month):
-    if month in [12, 1, 2]:
-        return "Summer"
-    elif month in [3, 4, 5]:
-        return "Autumn"
-    elif month in [6, 7, 8]:
-        return "Winter"
+    if df_list:
+        combined_df = pd.concat(df_list, ignore_index=True)
+        return combined_df
     else:
-        return "Spring"
-
-if __name__ == "__main__":
-    data = load_temperature_data("Resources/Temparatures")
-    data['Month'] = pd.to_datetime(data['Date']).dt.month
-    data['Season'] = data['Month'].apply(assign_season)
-    print(data[['Date', 'Month', 'Season']].head())
+        return pd.DataFrame()
